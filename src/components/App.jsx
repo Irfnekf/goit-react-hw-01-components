832976837ddd4139243d4e5434004ffbdf03fce1
Irfnekf <vladshkaraputa07@gmail.com>
2023-01-09 23:43:52 +0200

    the third task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 import User from '../data/user.json';
 import Data from '../data/data.json';
+import Friends from '../data/friends.json';
 const { username, tag, location, avatar, stats } = User;
 
 export const App = () => {
@@ -16,6 +18,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={Data} />
       <Statistics stats={Data} />
+      <FriendList friends={Friends} />;
     </>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import css from '../FriendList/FriendList.module.css';
+import PropTypes from 'prop-types';
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+
+export function FriendList({ friends }) {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          id={id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  Friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
