baa0979d17bb88a9295ada0981b2caca59a290e0
Irfnekf <vladshkaraputa07@gmail.com>
2023-01-10 22:01:07 +0200

    the fourth task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import User from '../data/user.json';
 import Data from '../data/data.json';
 import Friends from '../data/friends.json';
+import Transactions from '../data/transactions.json';
 const { username, tag, location, avatar, stats } = User;
 
 export const App = () => {
@@ -18,7 +20,8 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={Data} />
       <Statistics stats={Data} />
-      <FriendList friends={Friends} />;
+      <FriendList friends={Friends} />
+      <TransactionHistory items={Transactions} />
     </>
   );
 };
